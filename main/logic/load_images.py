19dde9a83b9a4f98f26f3a9dd8ebe75ecfdbac1c
ilakorotkov <kila280509@gmail.com>
2025-01-01 03:04:53 +0300

    добавление функции загрузки изображения

---
+++ main/logic/load_images.py
@@ -0,0 +1,21 @@
+"""Загрузка изображения"""
+# Импорт библиотек
+import pygame
+import os
+
+
+def load_image(name, color_key=None):   # Функция для загрузки изображения
+    fullname = os.path.join('main', 'images', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
