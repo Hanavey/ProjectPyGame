c4c4af68ba4240a75137939ae9a159e4eec15fb4
ilakorotkov <kila280509@gmail.com>
2025-01-06 19:56:39 +0300

    изменение логики начального экрана

--- init.py
+++ init.py
@@ -1,38 +1,100 @@
 """Основной файл, запускающий игру."""
-import os.path
-
 # Импорт библиотек
 import pygame
-import os
+import sys
 # Импорт созданных классов и функций
-from main.logic.board import Board
-from main.logic.home_screen import HomeScreen
-from main.logic.load_images import load_image
+from main.logic.button import Button
+from main.logic.text import Text
+from time import sleep
+from main.logic.fade_in_out import fade_in_out
+
+
+screen = pygame.display.set_mode((1920, 1080))
+
+
+def option() -> None:
+    screen.fill((255, 255, 255))
+    Text(font_size=100, color=(0, 0, 0)).render(screen, 'Пока идет разработка(((', (960, 100), True)
+    btn_return = Button(screen, (720, 300), (480, 50), text='Назад', text_color=(0, 0, 0))
+    btn_return.render()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1 and btn_return.get_click(event.pos):
+                    return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
+        pygame.display.update()
+
+
+def play() -> None:
+    screen.fill((0, 0, 0))
+    Text(font_size=100, color=(255, 255, 255)).render(screen, 'Пока идет разработка(((', (960, 100), True)
+    btn_return = Button(screen, (720, 300), (480, 50), text='Назад', text_color=(255, 255, 255))
+    btn_return.render()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1 and btn_return.get_click(event.pos):
+                    return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
+        pygame.display.update()
+
+
+def quit_screen(screen_start) -> None:
+    scr = fade_in_out(screen, screen_start, fade_in=False, max_alpha=122, speed=3)
+    screen.blit(scr, (0, 0))
+    no_btn = Button(screen, (460, 540), (400, 50), text='NO', image='main/images/Drawing (1).png', surface=screen)
+    yes_btn = Button(screen, (1000, 540), (400, 50), text='YES', image='main/images/Drawing (1).png', surface=screen)
+    Text(color=(255, 0, 0), font_size=100).render(screen, 'Выйти из игры?', (500, 360))
+    no_btn.render()
+    yes_btn.render()
+    pygame.display.flip()
+    while True:
+        for event_ in pygame.event.get():
+            if event_.type == pygame.QUIT:
+                break
+            if event_.type == pygame.MOUSEBUTTONDOWN:
+                if yes_btn.get_click(event_.pos):
+                    sleep(0.1)
+                    pygame.quit()
+                    sys.exit()
+                elif no_btn.get_click(event_.pos):
+                    fade_in_out(screen, screen_start, speed=3, min_alpha=122)
+                    return
 
 
 def Main():
     pygame.init()
-    screen = pygame.display.set_mode((1920, 1080))
     pygame.display.set_caption('Разрушитель лабиринтов')
-
     clock = pygame.time.Clock()
-
-    start_menu = HomeScreen(screen, 'home_screen.png', True)
-    el = start_menu.render(screen)
-
+    screen.fill((0, 0, 0))
+    screen_start = pygame.image.load('main/images/home_screen.png')
+    screen.blit(screen_start, (0, 0))
+    button_start = Button(screen, (720, 425), (480, 50), image='main/images/Drawing (1).png', surface=screen_start)
+    button_help = Button(screen, (720, 550), (480, 50), image='main/images/Drawing (1).png', surface=screen_start)
+    button_exit = Button(screen, (720, 825), (480, 50), image='main/images/Drawing (1).png', surface=screen_start)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        if el == 1:
-            screen.fill((0, 0, 0))
-        elif el == 2:
-            screen.fill((255, 255, 255))
-        else:
-            el = start_menu.render(screen)
-        pygame.display.update()
-        clock.tick(60)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                button_start.connect(play, event.pos)
+                button_help.connect(option, event.pos)
+                button_exit.connect(quit_screen, event.pos, (screen_start,))
+        button_start.render()
+        button_help.render()
+        button_exit.render()
     pygame.quit()
 
 
